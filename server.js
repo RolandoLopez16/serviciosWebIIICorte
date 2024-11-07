93ef935983059d7184d6c19e768393f0ae9bb490
Rolandolopez16 <rolandolopez@unicomfacauca.edu.co>
2024-11-07 09:38:05 -0500

    se cambuio la swagger option

--- server.js
+++ server.js
@@ -4,30 +4,28 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const swaggerJsDoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
-const cors = require('cors'); // Importar cors para manejar CORS
+const cors = require('cors'); 
 const conductoresRoutes = require('./routes/conductores');
 
 dotenv.config();
 
 const app = express();
 
-// Configuración de CORS para permitir solicitudes desde el frontend en localhost:3000
+// Configuración de CORS para permitir cualquier origen
 app.use(cors({
-  origin: '*', // Permitir cualquier origen
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'] // Permitir todos los métodos
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH']
 }));
-
 
 // Middleware para analizar el cuerpo de las solicitudes en formato JSON
 app.use(bodyParser.json());
 
 // Conexión a la base de datos MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-})
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((err) => console.error('Failed to connect to MongoDB', err));
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('Conectado a MongoDB'))
+  .catch((err) => console.error('Error al conectar a MongoDB', err));
 
-// Configuración de Swagger
+// Configuración de Swagger con la URL de producción en Vercel
 const swaggerOptions = {
   swaggerDefinition: {
     openapi: '3.0.0',
@@ -35,14 +33,23 @@ const swaggerOptions = {
       title: 'API de Conductores',
       version: '1.0.0',
       description: 'API para gestionar conductores',
+      contact: {
+        name: 'Rolando',
+        email: 'contacto@ejemplo.com'
+      },
+      license: {
+        name: 'MIT',
+        url: 'https://opensource.org/licenses/MIT'
+      }
     },
     servers: [
       {
-        url: `http://localhost:${process.env.PORT || 5000}`,
-      },
-    ],
+        url: 'https://apiservi-96i373zox-rolandos-projects-9705129c.vercel.app',
+        description: 'Servidor de Producción en Vercel'
+      }
+    ]
   },
-  apis: ['./routes/*.js'], // Asegúrate de que la ruta sea correcta
+  apis: ['./routes/*.js']
 };
 
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
@@ -54,5 +61,5 @@ app.use('/api/conductores', conductoresRoutes);
 // Puerto de escucha para el servidor
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
